5ee250aed0ffe79d86db14093b13f4fe5d0d85be
James Cacapit <jamescacapit@gmail.com>
2025-10-01 21:18:11 -0700

    add doc file and initial setup

--- db.py
+++ db.py
@@ -1,10 +1,14 @@
-from sqlmodel import Session, create_engine
+from sqlmodel import SQLModel, Session, create_engine
 
 # name of the database to connect to
 db = "users"
 
-mysql_url = f"mysql+pymysql://root:password@IP_address:3306/{db}"
+
+
+
+mysql_url = f"mysql+pymysql://root:xcr223@152.42.164.213:3306/{db}"
 engine = create_engine(mysql_url, echo=True)
+SQLModel.metadata.create_all(engine)
 
 def get_session():
     with Session(engine) as session:
